002becd803c27ad83996af22e2cd92998d54565a
Abutalif <abutaliph@gmail.com>
2024-12-18 19:40:46 +0500

    Init cargo

---
+++ shell/src/main.rs
@@ -0,0 +1,17 @@
+use std::io::{self, Write};
+
+fn main() {
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        if input.trim() == "exit" {
+            break;
+        }
+
+        println!("Hello there!");
+    }
+}
